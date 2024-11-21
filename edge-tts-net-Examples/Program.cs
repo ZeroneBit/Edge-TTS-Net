048ee06483c23927190cdb0cd2dc4b97c2df7f7e
ZeroneBit <kaida.zhao@outlook.com>
2024-11-21 21:14:58 +0800

    Package to nuget

--- edge-tts-net-Examples/Program.cs
+++ edge-tts-net-Examples/Program.cs
@@ -1,7 +1,5 @@
 ﻿using edge_tts_net;
 
-const string proxy = "";
-
 static async Task GetVoices()
 {
     var edgetts = new EdgeTTSNet();
@@ -11,7 +9,7 @@ static async Task GetVoices()
 
 static async Task BasicSave()
 {
-    var edgeTts = new EdgeTTSNet(webProxy: proxy);
+    var edgeTts = new EdgeTTSNet();
     await edgeTts.Save(@"Hello, World!", "basic.mp3");
 }
 
@@ -19,7 +17,7 @@ static async Task BasicStream()
 {
     var fs = new FileStream("basic_stream.mp3", FileMode.Create);
 
-    var edgetts = new EdgeTTSNet(webProxy: proxy);
+    var edgetts = new EdgeTTSNet();
     await edgetts.TTS(@"Hello, World!", (metaObj) =>
     {
         if (metaObj.Type == TTSMetadataType.Audio)
@@ -32,9 +30,28 @@ static async Task BasicStream()
     fs.Dispose();
 }
 
+static async Task SetOptions()
+{
+    var edgetts = new EdgeTTSNet();
+
+    var voices = await edgetts.GetVoices();
+    var cnVoice = voices.FirstOrDefault(v => v.Locale == "zh-CN");
+    var options = new TTSOption
+    (
+        voice: cnVoice.Name,
+        pitch: "+0Hz",
+        rate: "+25%",
+        volume: "+0%"
+    );
+
+    await edgetts.Save("Hello, World", "basic_option.mp3");
+}
+
 
 await GetVoices();
 
 await BasicSave();
 
 await BasicStream();
+
+await SetOptions();
